b0bd041b8ec998b7c9077f30dbc0456d41be46dd
Niko Lehtovirta <niko.lehtovirta@nordcloud.com>
2020-02-18 14:30:26 +0200

    Add testing framework and setup test in pipeline

---
+++ lib/monitoring/lib/generator.spec.ts
@@ -0,0 +1,7 @@
+import test from 'ava';
+
+import * as generator from './generator';
+
+test('generatePath returns path as string', (t) => {
+  t.true(typeof(generator.generatePath('test')) === 'string');
+})
