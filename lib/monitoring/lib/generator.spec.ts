b5361de32e3b92e79bec3fda17365191f86b84a1
Niko Lehtovirta <niko.lehtovirta@nordcloud.com>
2020-02-18 14:32:19 +0200

    Fix linter error in test

--- lib/monitoring/lib/generator.spec.ts
+++ lib/monitoring/lib/generator.spec.ts
@@ -2,6 +2,6 @@ import test from 'ava';
 
 import * as generator from './generator';
 
-test('generatePath returns path as string', (t) => {
-  t.true(typeof(generator.generatePath('test')) === 'string');
-})
+test('generatePath returns path as string', t => {
+  t.true(typeof generator.generatePath('test') === 'string');
+});
