4129e3952035c7a112e2d13e8fdb942a95f151da
Niko Lehtovirta <niko.lehtovirta@nordcloud.com>
2020-03-19 10:33:45 +0200

    chore: Move tests to test folder

--- test/monitoring/lib/generator.spec.ts
+++ test/monitoring/lib/generator.spec.ts
@@ -1,6 +1,6 @@
 import test from 'ava';
 
-import * as generator from './generator';
+import * as generator from '@monitoring/generator';
 
 test('generatePath returns path as string', t => {
   t.true(typeof generator.generatePath('test') === 'string');
