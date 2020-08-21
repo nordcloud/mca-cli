3fcae32f35c644ded0b430d2202e11758dd786fb
Viktor Hegyvári <viktor.hegyvari@nordcloud.com>
2020-08-21 10:10:32 +0300

    Updated utils index file

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,3 +1,4 @@
 export { default as chunk } from './chunk';
 export { default as validateCommand } from './validate-command';
 export { default as match } from './match';
+export { default as updatePagerDutyEndpoints } from './endpoints';
