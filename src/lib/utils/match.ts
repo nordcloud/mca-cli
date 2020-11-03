5a6f4033a2e23a220fbd276af35054cefe87a9be
Niko Lehtovirta <niko.lehtovirta@nordcloud.com>
2020-11-03 11:43:41 +0200

    chore: Remove not needed undefined check

--- src/lib/utils/match.ts
+++ src/lib/utils/match.ts
@@ -6,7 +6,7 @@ function fixMatchString(str: string): string {
 
 export default function match(str: string, include: string[], exclude: string[]): boolean {
   const matchStr = fixMatchString(str)
-  const included = (include || []).map((str) => fixMatchString(str))
-  const excluded = (exclude || []).map((str) => fixMatchString(str))
+  const included = include.map((str) => fixMatchString(str))
+  const excluded = exclude.map((str) => fixMatchString(str))
   return (included.length === 0 || isMatch(matchStr, included)) && (excluded.length === 0 || !isMatch(matchStr, excluded));
 }
