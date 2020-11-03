45106197b233a5509be33c8117114f4769ce3ab2
Niko Lehtovirta <niko.lehtovirta@nordcloud.com>
2020-11-03 11:57:53 +0200

    chore: Make match more readable by splitting

--- src/lib/utils/match.ts
+++ src/lib/utils/match.ts
@@ -6,9 +6,18 @@ function fixMatchString(str: string): string {
   return str.replace(/\//g, '-');
 }
 
+function matchIncluded(str: string, included: string[]): boolean {
+  return included.length === 0 || isMatch(str, included);
+}
+
+function matchExcluded(str: string, excluded: string[]): boolean {
+  return excluded.length === 0 || !isMatch(str, excluded);
+}
+
 export default function match(str: string, include: string[], exclude: string[]): boolean {
-  const matchStr = fixMatchString(str)
-  const included = include.map((str) => fixMatchString(str))
-  const excluded = exclude.map((str) => fixMatchString(str))
-  return (included.length === 0 || isMatch(matchStr, included)) && (excluded.length === 0 || !isMatch(matchStr, excluded));
+  const matchStr = fixMatchString(str);
+  const included = include.map(str => fixMatchString(str));
+  const excluded = exclude.map(str => fixMatchString(str));
+
+  return matchIncluded(matchStr, included) && matchExcluded(matchStr, excluded);
 }
