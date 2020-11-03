80cae32fa895ed196de1e740364d94d75dd0f676
Niko Lehtovirta <niko.lehtovirta@nordcloud.com>
2020-11-03 11:25:12 +0200

    fix: Match properly when using / in include/exclude

--- src/lib/utils/match.ts
+++ src/lib/utils/match.ts
@@ -1,6 +1,12 @@
 import { isMatch } from 'micromatch';
 
+function fixMatchString(str: string): string {
+  return str.replace(/\//g, '-');
+}
+
 export default function match(str: string, include: string[], exclude: string[]): boolean {
-  const matchStr = str.replace(/\//g, '-');
-  return (include.length === 0 || isMatch(matchStr, include)) && (exclude.length === 0 || !isMatch(matchStr, exclude));
+  const matchStr = fixMatchString(str)
+  const included = (include || []).map((str) => fixMatchString(str))
+  const excluded = (exclude || []).map((str) => fixMatchString(str))
+  return (included.length === 0 || isMatch(matchStr, included)) && (excluded.length === 0 || !isMatch(matchStr, excluded));
 }
