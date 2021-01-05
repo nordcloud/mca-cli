ce8dee7e14c370e4b53592554402f6880623b4c9
Niko Lehtovirta <niko.lehtovirta@nordcloud.com>
2021-01-05 12:45:09 +0200

    feat: [MCA-729] CloudWatch alarms export (#12)
    * feat: Add new cloudwatch export command
    
    This command will fetch all cloudwatch metric
    alarms and export them to readable json format
    
    Generated json can be used to export alarm info
    to other formats
    
    * chore: Fix linter errors
    
    * chore: Rename AlarmAcc interface to AlarmExport
    
    * Fix using incorrect alarm level for warning
    
    * chore: Modularize cloudwatch aws-sdk code

---
+++ src/cmd/cloudwatch/export.ts
@@ -0,0 +1,106 @@
+import { Argv } from 'yargs';
+import { promises as fs } from 'fs';
+
+import { aws } from '../../lib';
+import { setVerbose } from '../../lib/logger';
+
+export const command = 'export [options]';
+export const desc = 'Export cloudwatch info';
+export const builder = (yargs: Argv<{}>): Argv<{}> => {
+  return yargs.options({
+    p: {
+      alias: 'profile',
+      describe: 'AWS IAM Profile',
+      type: 'string',
+    },
+    r: {
+      alias: 'region',
+      describe: 'Target region',
+      type: 'string',
+    },
+    i: {
+      alias: 'include',
+      describe: 'List of included names',
+      type: 'array',
+      default: [],
+    },
+    e: {
+      alias: 'exclude',
+      describe: 'List of excluded names',
+      type: 'array',
+      default: [],
+    },
+    v: {
+      alias: 'verbose',
+      describe: 'Set verbose logging',
+      type: 'boolean',
+      default: false,
+    },
+    o: {
+      alias: 'output',
+      describe: 'Path to generated json file',
+      type: 'string',
+      default: 'alarms.json',
+    },
+  });
+};
+
+interface Args {
+  profile?: string;
+  region?: string;
+  verbose?: boolean;
+  include: string[];
+  exclude: string[];
+  output: string;
+}
+
+interface AlarmExport {
+  [level: string]: {
+    [alarmName: string]: {
+      [alarmType: string]: {
+        threshold: number;
+        period: number;
+        comparisonOperator: string;
+        statistic: string;
+        description: string;
+      };
+    };
+  };
+}
+
+export const handler = async (args: Args): Promise<void> => {
+  setVerbose(args.verbose);
+  await aws.setAWSCredentials(args.profile, args.region);
+
+  const alarms = await aws.getCloudWatchMetricAlarms(args.include, args.exclude);
+  const parsed = alarms.reduce((acc, alarm) => {
+    const nameSplit = (alarm.AlarmName || '').split('-');
+
+    let alarmLevel = 'critical';
+    if (['critical', 'warning'].includes(nameSplit[nameSplit.length - 1])) {
+      alarmLevel = nameSplit.pop() as string;
+    }
+
+    const alarmType = nameSplit.pop() as string;
+    const name = nameSplit.join('-');
+
+    return {
+      ...acc,
+      [alarmLevel]: {
+        ...(acc[alarmLevel] || {}),
+        [name]: {
+          ...(acc[alarmLevel]?.[name] || {}),
+          [alarmType]: {
+            threshold: alarm.Threshold as number,
+            period: alarm.Period as number,
+            comparisonOperator: alarm.ComparisonOperator as string,
+            statistic: alarm.Statistic,
+            description: alarm.AlarmDescription || '',
+          },
+        },
+      },
+    } as AlarmExport;
+  }, {} as AlarmExport);
+
+  await fs.writeFile(args.output, JSON.stringify(parsed, null, 2));
+};
---
+++ src/lib/aws-sdk/cloudwatch.ts
@@ -0,0 +1,48 @@
+import * as AWS from 'aws-sdk';
+import { validateCredentials } from './credentials';
+import { debug } from '../logger';
+import { match } from '../utils';
+
+export async function getCloudWatchAlarms(
+  include?: string[],
+  exclude?: string[],
+): Promise<{ metricAlarms: AWS.CloudWatch.MetricAlarms, compositeAlarms: AWS.CloudWatch.CompositeAlarms }> {
+  validateCredentials();
+
+  const cw = new AWS.CloudWatch();
+
+  const metricAlarms: AWS.CloudWatch.MetricAlarms = [];
+  const compositeAlarms: AWS.CloudWatch.MetricAlarms = [];
+
+  debug('Getting CloudWatch metric alarms');
+  let NextToken: string | undefined;
+  do {
+    const res = await cw.describeAlarms({ NextToken }).promise();
+    metricAlarms.push(...(res.MetricAlarms || []));
+    compositeAlarms.push(...(res.CompositeAlarms || []));
+
+    NextToken = res.NextToken;
+  } while (NextToken);
+  debug(`Found ${metricAlarms.length} metric and ${compositeAlarms.length} composite alarms`);
+
+  return {
+    metricAlarms: metricAlarms.filter(alarm => match(alarm.AlarmName || '', include || [], exclude || [])),
+    compositeAlarms: compositeAlarms.filter(alarm => match(alarm.AlarmName || '', include || [], exclude || [])),
+  }
+}
+
+export async function getCloudWatchMetricAlarms(
+  include?: string[],
+  exclude?: string[],
+): Promise<AWS.CloudWatch.MetricAlarms> {
+  const { metricAlarms } = await getCloudWatchAlarms(include, exclude);
+  return metricAlarms;
+}
+
+export async function getCloudWatchCompositeAlarms(
+  include?: string[],
+  exclude?: string[],
+): Promise<AWS.CloudWatch.CompositeAlarms> {
+  const { compositeAlarms } = await getCloudWatchAlarms(include, exclude);
+  return compositeAlarms;
+}
